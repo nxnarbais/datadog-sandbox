cae558bf1ff51322287a085a5cf3f9673f4938f5
Nicolas Narbais <nicolas.narbais@gmail.com>
2023-04-12 14:30:03 +0200

    Add example of monitors from list

--- terraform_datadog/company/dev/team_a/versions.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_providers {
-    datadog = {
-      source = "terraform-providers/datadog"
-    }
-  }
-  required_version = ">= 0.13"
-}
--- terraform_datadog/modules/monitors.service.latency/main.tf
+++ terraform_datadog/modules/monitors.service.latency/main.tf
@@ -1,30 +1,29 @@
-data "template_file" "message" {
-  template = file(
-    "${path.module}/message.tpl",
-  )
-
-  vars = {
-    notifications_alert = var.notifications.alert
-    notifications_warn = var.notifications.warn
-    notifications_recovery = var.notifications.recovery
-    notifications_default = var.notifications.default
-    notifications_no_data = var.notifications.no_data
-  }
-}
-
 resource "datadog_monitor" "service_latency" {
   type                = "metric alert"
   name                = "[Service] Latency is high on ${var.service.name}"
   query               = <<EOF
-avg(last_15m):avg:trace.${var.service.operation_name}.duration.by.service.${var.latency_percentile}{env:${var.env},service:${var.service.name}} > ${var.thresholds.alert}
+percentile(last_15m):${var.latency_percentile}:trace.${var.service.operation_name}{env:${var.env},service:${var.service.name}} > ${var.thresholds.alert}
+
 EOF
-  message = data.template_file.message.rendered
-  thresholds = {
+  message = templatefile(
+    "${path.module}/message.tpl",
+    {
+      notifications_alert = var.notifications.alert
+      notifications_warn = var.notifications.warn
+      notifications_recovery = var.notifications.recovery
+      notifications_default = var.notifications.default
+      notifications_no_data = var.notifications.no_data
+    }
+  )
+
+  monitor_thresholds {
     critical          = var.thresholds.alert
     warning           = var.thresholds.warn
   }
+
   notify_no_data      = true
   no_data_timeframe   = 30
+
   include_tags        = true
   tags                = concat(["standard:true", "terraform:true", "env:${var.env}", "service:${var.service.name}", "owner:${var.owner}"], var.tags)
 }
