f1a4e54c25de573109896b3a254270e7b0d65842
Nicolas Narbais <nicolas.narbais@gmail.com>
2023-05-16 12:32:35 +0200

    Add new service app with dependency

---
+++ app_nodejs-fastify-twoapps/app2/server.js
@@ -0,0 +1,29 @@
+// This line must come before importing any instrumented module.
+const tracer = require('dd-trace').init({
+    logInjection: true,
+});
+  
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true })
+
+fastify.get('/', async (request, reply) => {
+    return { hello: 'world', step: 'The cake is a lie' }
+})
+  
+  
+fastify.get('/do-something', async (request, reply) => {
+    return { route: 'do-something', step: 'Did some hard work.' }
+})
+  
+/**
+ * Run the server!
+ */
+const start = async () => {
+    try {
+      await fastify.listen({ port: 3000, host: '0.0.0.0' })
+    } catch (err) {
+      fastify.log.error(err)
+      process.exit(1)
+    }
+  }
+  start()
